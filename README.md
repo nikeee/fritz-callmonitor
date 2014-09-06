a300edca5719c3d99f70ccb5b948eaa7a3d0b101
Niklas Mollenhauer <nikeee@outlook.com>
2014-09-06 21:57:29 +0200

    Rename

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 ﻿# node-fritzbox
 
+Sample:
+```
+"use strict";
 
+import fb = require("./CallMonitor");
+
+var cm = new fb.CallMonitor("192.168.178.1", 1012);
+
+cm.on("ring", rr =>
+{
+    console.dir(rr);
+    console.log(rr.caller + " calling...");
+});
+
+cm.on("call", rr => console.dir(rr));
+cm.on("pickup", rr => console.dir(rr));
+cm.on("hangUp", rr => console.dir(rr));
+
+cm.on("close", () => console.log("Connection closed."));
+cm.on("connect", () => console.log("Connected to device."));
+
+cm.connect();
+```
---
+++ line-readable-stream.d.ts
@@ -0,0 +1,38 @@
+﻿/**
+* @autor Niklas Mollenhauer <holzig@outlook.com>
+* @autor Philip Tellis <philip@bluesmoon.info>
+* @example
+* // creates a new instance
+* var lis = new LineReadableStream(underlyingStream);
+*/
+import events = require("events");
+import Stream = require("stream");
+declare class LineReadableStream extends Stream.Readable {
+    public underlyingStream : Stream.Readable;
+    public readable : boolean;
+    public delimiter : string;
+    private _underlyingStream;
+    private _data;
+    private _delimiter;
+    private static _implementedEvents;
+    /**
+    * @constructor
+    */
+    constructor(underlyingStream: Stream.Readable);
+    /**
+    * @constructor
+    */
+    constructor(underlyingStream: Stream.Readable, delimiter: string);
+    private initializeEvents();
+    /**
+    * Start overriding EventEmitter methods so we can pass through to underlyingStream If we get a request for an event we don't know about, pass it to the underlyingStream
+    */
+    public on(type: string, listener: Function): events.EventEmitter;
+    public addListener(type: string, listener: Function): events.EventEmitter;
+    public removeListener(type: string, listener: Function): events.EventEmitter;
+    public removeAllListeners(type: string): events.EventEmitter;
+    public resume(): LineReadableStream;
+    public pause(): LineReadableStream;
+    public setEncoding(encoding: string): LineReadableStream;
+}
+export = LineReadableStream;
