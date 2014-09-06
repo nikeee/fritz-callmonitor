9bd0c08cccc931c2ba8e03d1f24367546071ee1e
Niklas Mollenhauer <nikeee@outlook.com>
2014-09-06 16:52:16 +0200

    Konichiwa commit

---
+++ app.ts
@@ -0,0 +1,16 @@
+﻿"use strict";
+
+import fb = require("./CallMonitor");
+import util = require("util");
+
+var cm = new fb.CallMonitor("192.168.178.1", 1012);
+
+cm.on("ring", rr =>
+{
+    console.log(util.format(rr));
+    console.log(rr.callerId + " calling...");
+});
+
+cm.on("call", rr => console.log(util.format(rr)));
+cm.on("pickup", rr => console.log(util.format(rr)));
+cm.on("hangUp", rr => console.log(util.format(rr)));
