32f2d31d47af9a97bbb92407cb16a8099a31d1b0
Niklas Mollenhauer <nikeee@outlook.com>
2016-10-04 16:40:53 +0200

    Using tagged unions

--- src/fritz-callmonitor.ts
+++ src/fritz-callmonitor.ts
@@ -1,12 +1,12 @@
 ﻿import { LineStream, createStream } from "byline";
-import * as net from "net";
+import { Socket, connect as connectSocket } from "net";
 import * as moment from "moment";
 import { EventEmitter } from "events";
 
 export class CallMonitor extends EventEmitter {
 	private _reader: LineStream;
 	private _connected: boolean = false;
-	private _socket: net.Socket;
+	private _socket: Socket;
 
 	constructor(host: string);
 	constructor(host: string, port: number);
@@ -18,14 +18,14 @@ export class CallMonitor extends EventEmitter {
 		if (this._connected)
 			return;
 
-		this._socket = net.connect(this.port, this.host);
+		this._socket = connectSocket(this.port, this.host);
 		this._socket.on("connect", args => this.emit("connect", args));
 		this._socket.on("end", args => this.emit("end", args));
 		this._socket.on("timeout", args => this.emit("timeout", args));
 		this._socket.on("error", err => this.emit("error", err));
 		this._socket.on("close", args => this.emit("close", args));
 
-		this._reader = createStream(this._socket, { encoding: "utf-8" });
+		this._reader = createStream(this._socket as NodeJS.ReadableStream, { encoding: "utf-8" });
 		this._reader.on("data", (l: string) => this.processLine(l));
 
 		this._connected = true;
@@ -35,81 +35,29 @@ export class CallMonitor extends EventEmitter {
 		this._socket.end();
 	}
 
-	private processLine(line: string): void {
+	private processLine(line: string): boolean {
 		const data = this.parseLine(line);
-		if (!data)
-			return;
+		if (data === null)
+			return false;
 
+		// super.emit("phoneevent", data);
 		switch (data.eventType) {
-			case EventType.Ring:
-				return this.ring(data);
-			case EventType.Call:
-				return this.call(data);
-			case EventType.Pickup:
-				return this.pickup(data);
-			case EventType.HangUp:
-
-				return this.hangUp(data);
-			default:
-				return;
+			case EventType.Ring: return super.emit("ring", data);
+			case EventType.Call: return super.emit("call", data);
+			case EventType.PickUp: return super.emit("pickup", data);
+			case EventType.HangUp: return super.emit("hangup", data);
+			default: return false;
 		}
 	}
 
+	// public on(event: "phoneevent", listener: (data: PhoneEvent) => void): this;
 	public on(event: "call", listener: (data: CallEvent) => void): this;
 	public on(event: "ring", listener: (data: RingEvent) => void): this;
-	public on(event: "pickup", listener: (data: PickupEvent) => void): this;
+	public on(event: "pickup", listener: (data: PickUpEvent) => void): this;
 	public on(event: "hangup", listener: (data: HangUpEvent) => void): this;
 	public on(event: string, listener: Function): this;
 	public on(event: string, listener: Function): this {
 		return <this>super.on(event, listener);
-	}
-
-	private ring(data: CallEventData): void {
-		const ev: RingEvent = {
-			originalData: data.originalData,
-			date: data.date,
-			eventType: data.eventType,
-			connectionId: data.connectionId,
-			caller: data.fourth,
-			callee: data.fifth
-		};
-		super.emit("ring", ev);
-	}
-
-	private call(data: CallEventData): void {
-		const ev: CallEvent = {
-			originalData: data.originalData,
-			date: data.date,
-			eventType: data.eventType,
-			connectionId: data.connectionId,
-			extension: data.fourth,
-			caller: data.fifth,
-			callee: data.sixth
-		};
-		super.emit("call", ev);
-	}
-
-	private pickup(data: CallEventData): void {
-		const ev: PickupEvent = {
-			originalData: data.originalData,
-			date: data.date,
-			eventType: data.eventType,
-			connectionId: data.connectionId,
-			extension: data.fourth,
-			phoneNumber: data.fifth
-		};
-		super.emit("pickup", ev);
-	}
-
-	private hangUp(data: CallEventData): void {
-		const ev: HangUpEvent = {
-			originalData: data.originalData,
-			date: data.date,
-			eventType: data.eventType,
-			connectionId: data.connectionId,
-			callDuration: parseInt(data.fourth)
-		};
-		super.emit("hangUp", ev);
 	}
 
 	/*
@@ -126,7 +74,52 @@ export class CallMonitor extends EventEmitter {
 	Date;DISCONNECT;ConnectionId;DurationInSeconds;
 	*/
 
-	private parseLine(line: string): CallEventData {
+	private createEvent(eventType: EventType, date: Date, connectionId: number, line: string, splitLines: string[]): PhoneEvent {
+		switch (eventType) {
+			case EventType.HangUp:
+				return {
+					eventType: eventType,
+					originalData: line,
+					date: date,
+					connectionId: connectionId,
+
+					callDuration: parseInt(splitLines[3])
+				};
+			case EventType.Call:
+				return {
+					eventType: eventType,
+					originalData: line,
+					date: date,
+					connectionId: connectionId,
+
+					extension: splitLines[3],
+					caller: splitLines[4],
+					callee: splitLines[5]
+				};
+			case EventType.PickUp:
+				return {
+					eventType: eventType,
+					originalData: line,
+					date: date,
+					connectionId: connectionId,
+
+					extension: splitLines[3],
+					phoneNumber: splitLines[4]
+				};
+			case EventType.Ring:
+				return {
+					eventType: eventType,
+					originalData: line,
+					date: date,
+					connectionId: connectionId,
+
+					caller: splitLines[3],
+					callee: splitLines[4]
+				};
+		}
+	}
+
+	private parseLine(line: string): PhoneEvent | null {
 		if (!line)
 			return null;
 
@@ -138,85 +131,59 @@ export class CallMonitor extends EventEmitter {
 		const date = d.isValid() ? d.toDate() : new Date();
 
 		const evt = CallMonitor.eventTypeFromString(sp[1]);
-		if (evt === null)
-			return null; // invalid event type, return null
+		if (evt === undefined)
+			return null; // invalid event type
 
 		const connId = parseInt(sp[2]);
-
-		const res: CallEventData = {
-			originalData: line,
-			eventType: evt,
-			date: date,
-			connectionId: connId,
-			fourth: sp[3]
-		};
-
-		if (sp.length > 3)
-			res.fifth = sp[4];
-
-		if (sp.length > 4)
-			res.sixth = sp[5];
-
-		return res;
+		return this.createEvent(evt, date, connId, line, sp);
 	}
 
-	private static eventTypeFromString(ev: string): EventType {
+	private static eventTypeFromString(ev: string): EventType | undefined {
 		switch (ev.toUpperCase()) {
-			case "RING":
-				return EventType.Ring;
-			case "CALL":
-				return EventType.Call;
-			case "CONNECT":
-				return EventType.Pickup;
-			case "DISCONNECT":
-				return EventType.HangUp;
-			default:
-				return null; // Invalid event type, return null
+			case "RING": return EventType.Ring;
+			case "CALL": return EventType.Call;
+			case "CONNECT": return EventType.PickUp;
+			case "DISCONNECT": return EventType.HangUp;
+			default: return undefined;
 		}
 	}
 }
 
-interface CallEventData {
-	originalData: string;
-	eventType: EventType;
-	date: Date;
-	connectionId: number;
+export type PhoneEvent = RingEvent | CallEvent | HangUpEvent | PickUpEvent;
 
-	fourth: string;
-	fifth?: string;
-	sixth?: string;
-}
-
-export interface PhoneEvent {
-	eventType: EventType;
+export interface PhoneEventBase {
 	date: Date;
 	connectionId: number;
 	originalData: string;
 }
 
-export interface RingEvent extends PhoneEvent {
+export interface RingEvent extends PhoneEventBase {
+	eventType: EventType.Ring;
 	caller: string;
 	callee: string;
 }
 
-export interface CallEvent extends PhoneEvent {
+export interface CallEvent extends PhoneEventBase {
+	eventType: EventType.Call;
 	extension: string;
 	caller: string;
 	callee: string;
 }
 
-export interface PickupEvent extends PhoneEvent {
+export interface PickUpEvent extends PhoneEventBase {
+	eventType: EventType.PickUp;
 	extension: string;
 	phoneNumber: string;
 }
 
-export interface HangUpEvent extends PhoneEvent {
+export interface HangUpEvent extends PhoneEventBase {
+	eventType: EventType.HangUp;
 	callDuration: number;
 }
 
-export enum EventType {
-	Call,
-	Ring,
-	Pickup,
-	HangUp
+export const enum EventType {
+	Call = 0,
+	Ring = 1,
+	PickUp = 2,
+	HangUp = 4
 }
